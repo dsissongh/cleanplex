c16a3d7640e2310a0766ea7c0cc644b145f385fd
David Sisson <davidsissonemail@gmail.com>
2017-01-15 17:27:53 -0800

    Moves files to subdir\season.  Not a lot of other testing yet

--- cleanplex/functions.py
+++ cleanplex/functions.py
@@ -1,4 +1,5 @@
 import os
+import re
 from progressbar import ProgressBar
 
 def confighelper(oconfig, item):
@@ -25,6 +26,7 @@ def getlistoffileitems(rootpath):
 	pbar = ProgressBar()
 	print("Scanning root directory")
 	quickrun = 0 # remove later
+	#fh = open("filestodelete.txt","w")
 	for item in pbar(fileitems):
 		if os.path.isdir(rootpath + "\\" + item):
 			isshow = False
@@ -37,11 +39,14 @@ def getlistoffileitems(rootpath):
 				shows.append(item)
 			else:
 				nonshows.append(item)
+				#fh.write(item + "\n")
 			quickrun += 1
 			if quickrun > 10000:
 				break
 
+	#fh.close()			
 	return shows, nonshows		
+
 
 def getmediafile(rootpath, fileitem, acceptedfiletypes, disallowed):
 	'''
@@ -60,3 +65,53 @@ def getmediafile(rootpath, fileitem, acceptedfiletypes, disallowed):
 				rfiles.append(file)
 
 	return rfiles
+
+
+def getmediainfo(fileitem):
+    #tv = re.findall(r"(.*?)[ |.]S([\d+]{1,2})E([\d+]{1,2})[ |.]([\d+]{3,4}p|)", fileitem)
+    tv = re.findall(r"(.*?)[ |.|-][S|s]([\d+]{1,2})(|.)[E|e]([\d+]{1,2})[ |.|-]([\d+]{3,4}p|)", fileitem)
+    if len(tv) > 0:
+        tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+        return tv
+    else:
+        #tv = re.findall(r"(.*?)[ |.]([\d+]{4})\.([\d+]{2})\.\d{2}", fileitem)
+        tv = re.findall(r"(.*?)[ |.]([\d+]{4})\.([\d+]{2})\.\d{2}", fileitem)
+        if len(tv) > 0:
+            tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+            return tv
+        else:
+            #look for pattern "name.name.sxe.blah.blah.blah"  
+            tv = re.findall(r"(.*?)[ |.|-]([\d+]{1,2})[X|x]([\d+]{1,2})[ |.|-]([\d+]{3,4}p|)", fileitem)
+            if len(tv) > 0:
+                tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+                return tv
+            else:
+                return []
+
+
+def fixseason(season):
+    newseason = ""
+    if season[0] == "0":
+        newseason = season[1:]
+    else:
+        newseason = season
+    return newseason
+
+
+def getlistofpossibletitles(fileitem,shows):
+    """
+        Create list of possible names for the directory based on the original filename
+
+        Args:
+            fileitem    - title of the media item.  Used to determine what show is in the subdirectory
+            shows       - List of all the media directories (which should represent shows)
+        Returns:
+            A python list of possible media directory names.
+    """
+    title = []
+    title.append(fileitem)
+    lookfor = fileitem.replace("."," ")
+    title.append(lookfor)
+    lookfor = fileitem.replace('-'," ")
+    title.append(lookfor)
+    return title
--- cleanplex/main.py
+++ cleanplex/main.py
@@ -1,11 +1,15 @@
 
 import os
+import shutil
 from configparser import SafeConfigParser 
 from progressbar import ProgressBar
 from media import Media
 from functions import confighelper
 from functions import getlistoffileitems
 from functions import getmediafile
+from functions import getmediainfo
+from functions import fixseason
+from functions import getlistofpossibletitles
 
 #setup the config object
 config = SafeConfigParser()
@@ -23,17 +27,97 @@ disallowed = sdisallowed.split(',')
 pbar = ProgressBar()
 print("Scanning non show items")
 fh = open("nonshows.txt", 'w')
+countshowmove = 0
+countshowunfound = 0
+createsubdir = 0
+move = 0
+fileinfo = []
 for item in pbar(nonshowitems):
 	mediafile = getmediafile(rootpath, item, filetypes, disallowed)
-	#print(item.encode("utf-8"))
-	#print(mediafile)
+	if len(mediafile) > 0:
+		fileinfo = getmediainfo(mediafile[0])
+
 	fh.write(60*"-")
 	fh.write("\n")
 	fh.write(item)
 	fh.write("\n")	
 	fh.write(str(mediafile))
 	fh.write("\n")	
+	if len(fileinfo) > 0:
+		fh.write(str(fileinfo))
+		fh.write("\n")	
 
+	#show name, season and episode
+	shownames = []
+	if len(fileinfo) > 0:
+		showname = fileinfo[0][0]
+		season = fileinfo[0][1]
+		season = fixseason(season)
+		episode = fileinfo[0][2]
+		shownames = getlistofpossibletitles(showname, showitems)
+		for name in shownames:
+			showpath = rootpath +  name 	#+ "\\\\Season " + season
+			
+			if os.path.isdir(showpath):
+				showpath = rootpath +  name 	+ "\\\\Season " + season
+				if os.path.isdir(showpath):
+					showpathexist = True
+					showname = name 
+					countshowmove += 1
+					break
+				else:
+					#the season directory is missing
+					showpathexist = True
+					showname = name 
+					countshowmove += 1
+					fh.write("create season dir\n")
+					os.mkdir(showpath)
+					createsubdir =+ 1
+					break
+			else:
+				showpathexist = False
+				
 
+		fh.write(showname)
+		fh.write("\n")	
+		fh.write(str(shownames))
+		fh.write("\n")
+		fh.write(season)
+		fh.write("\n")	
+		fh.write(episode)
+		fh.write("\n")	
+		fh.write(showpath)
+		fh.write("\n")
+		fh.write(str(showpathexist))
+		fh.write("\n")
+		if showpathexist:
+			#move only if the show path exists
+			fh.write("Move: " + rootpath + item + "\\" + mediafile[0])
+			fh.write("\n")
+			fh.write("To: " + showpath + "\\" + mediafile[0])
+			fh.write("\n")
+
+			source = rootpath + item + "\\" + mediafile[0] 
+			destination = showpath + "\\" + mediafile[0]	
+			fh.write(source)
+			fh.write("\n")		
+			if not os.path.isfile(destination):
+				try:
+
+					shutil.move(source, destination)
+				except:
+					print("Could not move " + mediafile[0])
+					exit()
+				move += 1
+			else:
+				fh.write("Exists already")
+				os.remove(source)
+
+	fileinfo = []		
 fh.close()	
 
+print(str(countshowmove) + ":" + str(len(nonshowitems) - countshowunfound))
+print("\n")
+print(move)
+print("\n")
+print(createsubdir)

f683c568d2f6dc19705f3faa4bd9b63786dbc262
David Sisson <davidsissonemail@gmail.com>
2017-01-18 23:10:39 -0800

    Added media info

--- cleanplex/function2.py
+++ cleanplex/function2.py
@@ -1,4 +1,5 @@
 import os
+import re
 from progressbar import ProgressBar
 
 def confighelper(oconfig, item):
@@ -65,8 +66,30 @@ def interrogatedirectory(extensionlist, path, nonshow):
 	directory = os.listdir(path + nonshow)
 	for item in directory:
 		if item[-3:] in extensionlist:
-			show.append(item)
+			show.append([item, path + nonshow + "\\" + item])
 		else:
-			noshow.append(item)
+			noshow.append([item, path + nonshow + "\\" + item])
 
 	return show, noshow
+
+
+def getmediainfo(fileitem):
+    #tv = re.findall(r"(.*?)[ |.]S([\d+]{1,2})E([\d+]{1,2})[ |.]([\d+]{3,4}p|)", fileitem)
+    tv = re.findall(r"(.*?)[ |.|-][S|s]([\d+]{1,2})(|.)[E|e]([\d+]{1,2})[ |.|-]([\d+]{3,4}p|)", fileitem)
+    if len(tv) > 0:
+        tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+        return tv
+    else:
+        #tv = re.findall(r"(.*?)[ |.]([\d+]{4})\.([\d+]{2})\.\d{2}", fileitem)
+        tv = re.findall(r"(.*?)[ |.]([\d+]{4})\.([\d+]{2})\.\d{2}", fileitem)
+        if len(tv) > 0:
+            tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+            return tv
+        else:
+            #look for pattern "name.name.sxe.blah.blah.blah"  
+            tv = re.findall(r"(.*?)[ |.|-]([\d+]{1,2})[X|x]([\d+]{1,2})[ |.|-]([\d+]{3,4}p|)", fileitem)
+            if len(tv) > 0:
+                tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+                return tv
+            else:
+                return []
--- cleanplex/main2.py
+++ cleanplex/main2.py
@@ -4,6 +4,7 @@ from configparser import SafeConfigParser
 from function2 import confighelper
 from function2 import getlistoffileitems
 from function2 import interrogatedirectory
+from function2 import getmediainfo
 
 #setup the config object
 config = SafeConfigParser()
@@ -21,14 +22,42 @@ showdirs, nonshowdirs = getlistoffileitems(rootpath)
 #- move allowed file types to proper subdirectory
 #- remove disallowed file types 
 #- lastly, remove scrap directory
+count = 0
 for nonshow in nonshowdirs:
+	print(80*"-")
+
 	print(nonshow.encode("utf-8"))
 
 	#get list of accepted and rejected files from given directory
 	accepted, rejected = interrogatedirectory(filetypes, rootpath, nonshow)
 	print(accepted)
-	print(rejected)
-	exit()
+	print(40*"*")
+	for show in accepted:
+		info = []
+		print(show)
+		#lets get the media info for each file
+		#sub 0 = filename
+		#sub 1 = full path and filename
+		print(show[0])
+		info = getmediainfo(show[0])
+		if len(info) > 0:
+			print(info)
+		'''
+		print(len(accepted))
+		print(len(info))
+		if len(accepted) > 0:	
+			print("ACC: " + accepted[0][0])
+		if len(info) > 0:
+			print("INF: " + info)
+		'''	
+
+	count += 1
+	if count > 5:
+		exit()
+	#size = os.path.getsize(accepted[0])
+	#print(size)
+	#print(rejected)
+	
 
 print("Show directories: %d" % len(showdirs))
 print("Non show directories: %d" % len(nonshowdirs))

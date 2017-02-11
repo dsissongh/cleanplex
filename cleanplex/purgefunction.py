727a8d9844b3b829c7fa26f9a448c18b3945501f
David Sisson <davidsissonemail@gmail.com>
2017-02-10 22:37:09 -0800

    Adding directory traversal

--- cleanplex/purgefunction.py
+++ cleanplex/purgefunction.py
@@ -1,8 +1,10 @@
-
+import os
 
 def logactivity(fh, action, message):
 	fh.write(action.upper() + ": " + message + "\n")
 
 
 def traversedir(path):
-	pass
+	listing = os.listdir(path)
+	for item in listing:
+		pass

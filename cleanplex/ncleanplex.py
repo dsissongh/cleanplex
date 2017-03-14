d0f3710beb25537d8af38461a3af23b18972ef1c
David Sisson <davidsissonemail@gmail.com>
2017-03-14 00:09:13 -0700

    Correctly choosing directory for input elements

--- cleanplex/ncleanplex.py
+++ cleanplex/ncleanplex.py
@@ -1,19 +1,31 @@
 
 import os
+import shutil
 from natsort import natsorted
 
 from nfunc import checkshowdir
+from nfunc import getfileextensions
+from nfunc import getlistofpossibletitles
+from nfunc import getmediatype
+from nfunc import checkshowdirectory
 
 rootpath = "//mnt//h//TV//"
 ncleanplex = 'ncleanplex.log'
 
+fileebad = ['idx', 'mkv', 'sfv', 'exe', 'nzb', 'sub', 'srr', 'avi', 'mp4', 'nfo', 'jpg', 'srt']
+fileegood = ['mkv', 'avi', 'mp4']
+
 ncleanplexlog = open(ncleanplex, 'w+')
+shutil.copyfile("showtitles.txt", "showtitles.dat")
+showtitles = open("showtitles.txt", 'w+')
+notfound = open("notfound.txt", 'w+')
 
 disallowed = ['Thumbs.db', '_UNPACK_']
 
 directory = natsorted(os.listdir(rootpath))
 nonshow = 0
-
+allext = []
+newext = []
 
 allowed = True
 #loop through root directory
@@ -23,16 +35,50 @@ for item in directory:
 			allowed = False
 
 	if allowed:
-		check = checkshowdir(rootpath + item)
-		if not check:
-			print("notshowdir: " + item)
-			ncleanplexlog.write(item + "\n")
-			nonshow += 1
-		else:
-			pass
-			#print(item)
 
+		check = checkshowdir(rootpath + item)
+		#This is the main loop - if the item is not a show directory
+		if not check:
+			print("------------------------------------------------------------------\n")
+			ncleanplexlog.write("------------------------------------------------------------------\n")
+			print("notshowdir: " + item)
+			#ncleanplexlog.write(item + "\n")
+			nonshow += 1
+
+			#lets figure out what show it belongs too
+
+			info = getmediatype(item)
+			print(str(info[1]))
+			print(len(info[1]))
+			if len(info[1]) > 0:
+				print(info[1][0][0])
+				ncleanplexlog.write(info[1][0][0] + "\n")
+				titles = getlistofpossibletitles(info[1][0][0],"showtitles.dat")
+				print(str(titles))
+				ncleanplexlog.write(str(titles) + "\n")
+				actualtitle = checkshowdirectory(rootpath, titles)
+
+				if len(actualtitle) > 0:
+					print(actualtitle)
+					ncleanplexlog.write(actualtitle + "\n")
+				else:
+					notfound.write(info[1][0][0] + "\n")
+
+
+			extension = getfileextensions(rootpath + item)
+			print(str(extension))
+			allext = allext + extension
+		else:
+			#print(item)
+			showtitles.write(item + "\n")
+
+
+		
 	allowed = True
 
+newext = list(set(allext))
 print("NONSHOWDIR: %d" % nonshow)
+print(str(newext))
+
+showtitles.close()
 ncleanplexlog.close()
--- cleanplex/nfunc.py
+++ cleanplex/nfunc.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def checkshowdir(path):
 	showdir = False
@@ -8,3 +9,83 @@ def checkshowdir(path):
 			showdir = True
 
 	return showdir
+
+def getfileextensions(path):
+	ext = []
+	subdir = os.listdir(path)
+	for item in subdir:
+		if item[-4:-3] == '.':
+			ext.append(item[-3:])
+
+	return ext
+
+def getlistofpossibletitles(fileitem,fname):
+    """
+        Create list of possible names for the directory based on the original filename
+
+        Args:
+            fileitem    - title of the media item.  Used to determine what show is in the subdirectory
+            fname       - name of the inventory file.  This file has a list of all the media directories
+        Returns:
+            A python list of possible media directory names.
+    """
+    title = []
+    oddtitles = open("oddtitles.txt", 'r')
+    content = oddtitles.read()
+    oddtitles.close()
+
+    content = content.split("\n")
+    for line in content:
+        elements = line.split(',')
+        if fileitem.lower() in elements[0].lower():
+            #print(elements[1])
+            newfileitem = elements[1][1:len(elements[1])-1]
+            title.append(newfileitem)
+
+    
+    title.append(fileitem)
+    title.append(fileitem.title())
+    lookfor = fileitem.replace("."," ")
+    title.append(lookfor)
+    title.append(lookfor.title())
+    lookfor = fileitem.replace('-'," ")
+    title.append(lookfor)
+    title.append(lookfor.title())
+    with open(fname, "r") as dataf:
+        for line in dataf:
+            if lookfor.upper() in line.upper():
+                line = line.replace("\n","")
+                title.append(line)
+                title.append(line.title())
+    return title	
+
+
+def getmediatype(fileitem):
+	#tv = re.findall(r"(.*?)[ |.]S([\d+]{1,2})E([\d+]{1,2})[ |.]([\d+]{3,4}p|)", fileitem)
+	tv = re.findall(r"(.*?)[ |.|-][S|s]([\d+]{1,2})(|.)[E|e]([\d+]{1,2})[ |.|-]([\d+]{3,4}p|)", fileitem)
+	if len(tv) > 0:
+	    tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+	    return "mediadir",tv
+	else:
+	    #tv = re.findall(r"(.*?)[ |.]([\d+]{4})\.([\d+]{2})\.\d{2}", fileitem)
+	    tv = re.findall(r"(.*?)[ |.]([\d+]{4})\.([\d+]{2})\.\d{2}", fileitem)
+	    if len(tv) > 0:
+	        tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+	        return "mediadir",tv
+	    else:
+	        #look for pattern "name.name.sxe.blah.blah.blah"  
+	        tv = re.findall(r"(.*?)[ |.|-]([\d+]{1,2})[X|x]([\d+]{1,2})[ |.|-]([\d+]{3,4}p|)", fileitem)
+	        if len(tv) > 0:
+	            tv = [tuple(x if x is not None else x for x in _ if x) for _ in tv]
+	            return "mediadir",tv
+	        else:
+	            return "directory",[]
+
+
+def checkshowdirectory(path, titles):
+    newshow = ""
+    for filetocheck in titles:
+        if os.path.isdir(path + filetocheck):
+            newshow = path + filetocheck
+    
+    return newshow
